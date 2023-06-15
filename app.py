33cb149f7b5628205ff7d30a19826654bfbe687b
Shawn Robinson <shawnzr23@gmail.com>
2023-06-14 22:44:14 -0400

    Test

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 # Comment in Python
-for number in range(3):
-    print("Attempt", number + 1,)
+import random
+import string
+
+
+def pas():
+    first_letter = random.choice(string.ascii_letters) + random.choice(
+        string.ascii_letters) + random.choice(
+        string.ascii_letters) + random.choice(string.ascii_letters)
+    num = random.randint(1000, 9999)
+    password = first_letter + str(num)
+    print(password)
+
+
+pas()
